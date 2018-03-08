aaaaa2ee5b62fc0de45e8af71ea75d6a4daf39f2
BillGeoghegan <shadagascavich@gmail.com>
2018-03-08 20:58:44 +0000

    Added Expensify Styles

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -63,19 +63,20 @@ export default class ExpenseForm extends React.Component {
     };
     render() {
         return (
-            <div>
-                {this.state.error && <p>{this.state.error}</p>}
-                <form onSubmit={this.onSubmit}>
+                <form className="form" onSubmit={this.onSubmit}>
+                    {this.state.error && <p className="form__error">{this.state.error}</p>}
                     <input
                         type="text"
                         placeholder="Description"
                         autoFocus
+                        className="text-input"
                         value={this.state.description}
                         onChange={this.onDescriptionChange}
                     />
                     <input
                         type="number"
                         placeholder="Amount"
+                        className="text-input"
                         value={this.state.amount}
                         onChange={this.onAmountChange}
                     />
@@ -88,17 +89,20 @@ export default class ExpenseForm extends React.Component {
                         isOutsideRange={() => false}    //Make every day false without any logic test
                     />
                     <textarea
+                        className="textarea"
                         placeholder="Add a note for your expense (optional)"
                         value={this.state.note}
                         onChange={this.onNoteChange}
                     >
                     </textarea>
-                    <button
-                        type="submit"
-                        value="Submit"
-                    >Add Expense</button>
+                    <div>
+                        <button
+                            className="button"
+                            type="submit"
+                            value="Submit"
+                        >Save Expense</button>
+                    </div>
                 </form>
-            </div>
         )
     }
 }
